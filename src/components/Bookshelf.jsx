359d3c8b1d7377b63f63d1d7d544d218ca9e888a
haiaza <ajames1342@gmail.com>
2024-09-22 12:19:41 -0400

    removing the extra space

--- src/components/Bookshelf.jsx
+++ src/components/Bookshelf.jsx
@@ -14,43 +14,45 @@ const Bookshelf = () => {
 
         // handlers
 
-    const handleInputChange = (e) => {
-        // const bookUpdate = (e.target.value)
-        const { name, value } = e.target
+const handleInputChange = (e) => {
+    // const bookUpdate = (e.target.value)
+    const { name, value } = e.target
 
 
-        setNewBook((book) => ({
-            ...book, 
-            [name]: value, 
-        }))
-    }
+    setNewBook((book) => ({
+        ...book, 
+        [name]: value, 
+    }))
+}
 
-    return (
-        <>
-            <div className="bookshelfDiv">
-                <div className="formDiv">
-                    <h3>Add a Book</h3>
-                    <label htmlFor="newBook">Title</label>
-                    <input 
-                        id="newBook" 
-                        name="title"    
-                        type="text" 
-                        value={newBook.title} 
-                        onChange={handleInputChange}
-                    />
-                    <label htmlFor="newBook">Author</label>
-                    <input 
-                        id="newBook" 
-                        name="author" 
-                        type="text" 
-                        value={newBook.author} 
-                        onChange={handleInputChange}
-                    />
-                </div>
-                <div className="bookCardsDiv">{/* Book cards will display here */}</div>
+
+
+return (
+    <>
+        <div className="bookshelfDiv">
+            <div className="formDiv">
+                <h3>Add a Book</h3>
+                <label htmlFor="newBook">Title</label>
+                <input 
+                    id="newBook" 
+                    name="title"    
+                    type="text" 
+                    value={newBook.title} 
+                    onChange={handleInputChange}
+                />
+                <label htmlFor="newBook">Author</label>
+                <input 
+                    id="newBook" 
+                    name="author" 
+                    type="text" 
+                    value={newBook.author} 
+                    onChange={handleInputChange}
+                />
             </div>
-        </>
-    )
+            <div className="bookCardsDiv">{/* Book cards will display here */}</div>
+        </div>
+    </>
+)
 }
 
 export default Bookshelf
